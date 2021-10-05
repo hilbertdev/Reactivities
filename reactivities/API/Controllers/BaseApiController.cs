4ff4b31c431c3af8952071b09f42dfaa016fa466
hilbertmuna <hilbertmuna@gmail.com>
2021-10-05 15:28:55 +0200

    End of section 4

--- reactivities/API/Controllers/BaseApiController.cs
+++ reactivities/API/Controllers/BaseApiController.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks; 
 using Microsoft.AspNetCore.Mvc;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
 namespace API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
     public class BaseApiController : ControllerBase 
     {
+        private IMediator _mediator;
+        protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
     }
 }
---
+++ reactivities/Application/Core/MappingProfiles.cs
@@ -0,0 +1,18 @@
+using System.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Domain;
+
+namespace Application.Core
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<Domain.Activity, Domain.Activity>();
+        }
+    }
+}
